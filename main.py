7a01b192aa511ce4d0a7675155f5e2b677214665
Jason Washburn <jason.washburn@gmail.com>
2025-06-18 05:59:38 -0500

    feat: add agent loop

--- main.py
+++ main.py
@@ -168,21 +168,30 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
-response = client.models.generate_content(
-    model=MODEL,
-    contents=messages,
-    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
-)
+for _ in range(20):
 
-if response.function_calls:
-    for function_call_part in response.function_calls:
-        function_call_result = call_function(function_call_part, verbose=verbose_output)
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception(f"Function call failed: {function_call_part.name}")
-        verbose_print(f"-> {function_call_result.parts[0].function_response.response}")
+    response = client.models.generate_content(
+        model=MODEL,
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+    )
 
+    for candidate in response.candidates:
+        messages.append(candidate.content)
 
-verbose_print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-verbose_print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            function_call_result = call_function(function_call_part, verbose=verbose_output)
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception(f"Function call failed: {function_call_part.name}")
+            verbose_print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(function_call_result)
+
+        verbose_print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        verbose_print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    else:
+        break
+
 if response.text:
     print(response.text)
